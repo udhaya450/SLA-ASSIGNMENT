64a83f63f431a4838f3d0f48a65f6dc6b98f1d65
udhaya450 <udhaya062005@gmail.com>
2026-08-03 16:50:09 +0530

    Add files via upload

---
+++ day1 task(day15).js
@@ -0,0 +1,20 @@
+let name1="Udhaya U";
+let age=21;
+console.log(`Name is ${name1} and age is ${age}`);
+
+// 4.Write single-line and multi-line comments.
+// this is single line comment
+/*  this is 
+    mutliline comments */
+
+// 5.Declare variables using var, let, and const.
+var firstname="morgan";
+let myage=22;
+const percent=100;
+
+// 6.Create variables with string, number, boolean, null, undefined.
+let name2="Arun";
+let number=365;
+let bool=true;
+let variable=null;
+let nothing;//undefined
